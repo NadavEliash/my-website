6e1f63c9e9ab379f8f0e8a743bc798b809775c03
NadavEliash <nadav.eliash@gmail.com>
2024-05-22 11:18:48 +0300

    remove pdf route

--- app/(pages)/resume/page.tsx
+++ app/(pages)/resume/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 
-export default function Resume(){
+export default function Resume() {
     return (
         <div></div>
+        // <iframe src="https://drive.google.com/file/d/1ZaJTI-GByj5o4FIRtB2yXsgm92WZ_dIa/view?usp=sharing" className="w-full h-svh"></iframe>
     )
 }
