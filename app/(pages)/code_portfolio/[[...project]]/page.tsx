37f7939ea003904b4f9bee637f6b5486ed0716e8
NadavEliash <nadav.eliash@gmail.com>
2024-09-02 09:54:58 +0300

    edit and design projects

--- app/(pages)/code_portfolio/[[...project]]/page.tsx
+++ app/(pages)/code_portfolio/[[...project]]/page.tsx
@@ -14,6 +14,7 @@ export interface project {
     description: string[]
     src: string
     video: string
+    mobileVideo?: string 
     repo: string
 }
 
@@ -23,7 +24,7 @@ const projects: project[] = [
         title: 'Animate online',
         description: ['Animation app, based on HTML Canvas and React.', 'The app allow users create an animation clips from scratch. User can draw, erase, then translate, rotate and scale his drawings. Drawing along multiple frames creates animation, which the user can play and download.'],
         src: 'https://animate.nadaveliash.com/',
-        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1716328797/animate_dy2dk9.mp4',
+        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725219524/pc-animate_mccrt2.mp4',
         repo: 'https://github.com/NadavEliash/animate-online'
     },
     {
@@ -31,7 +32,8 @@ const projects: project[] = [
         title: 'Finerr',
         description: ['A Fiverr-like marketplace, contains the full user experience of the original app.', 'I take part of a small team which created it within three weeks, using React, Redux, SCSS, Node.js, MongoDB and Git', 'I was responsible for Homepage, search and filter components, as well as to create and connect the Server and Database'],
         src: 'https://finerr.onrender.com/',
-        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1715799738/finerr_xk2fx8.mp4',
+        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725219758/pc-finerr_m6ht9x.mp4',
+        mobileVideo: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725258027/mobile-finerr_rt00ku.mp4',
         repo: 'https://github.com/TodikPanshin/Sprint4-finerr'
     },
     {
@@ -41,15 +43,26 @@ const projects: project[] = [
             'The user\'s hebrew text sent to Google translate, then the translation sent to Openai or Replicate for generation.',
             'Each user get 5 sessions for free and then have to subscribe. The payment is made via Upay which give the user new link according their plan, which sign them in the database.'],
         src: 'https://ai-heb-app.vercel.app/dashboard',
-        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1715799717/heb-ai_xsylql.mp4',
+        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725219787/pc-bina_dgfimy.mp4',
+        mobileVideo: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725258045/mobile-bina_lxqciq.mp4',
         repo: 'https://github.com/NadavEliash/AI-heb-app'
+    },
+    {
+        params: 'trip-planner',
+        title: 'Trip planner',
+        description: ['Web app for planning trips.','The app connects with ChatGPT and Google Cloud via REST API to provide users with a complete trip plan, delivering a neat and clean user experience.','Springboot, JWT, Rest API (Chat GPT, Google Maps, Google Places), Mongodb, Docker, AWS, React.js'],
+        src: 'http://triplanner.nadaveliash.com/',
+        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725219515/pc-trip_lhsdqc.mp4',
+        mobileVideo: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725257991/mobile-trip_zkx45p.mp4',
+        repo: 'https://github.com/NadavEliash/trip-planner'
     },
     {
         params: 'crypto-share',
         title: 'Crypto share',
         description: ['Share with friends demo app, based on Vue.js.', 'The app allows user to check current value and statistics of Bitcoin. also, gives the users demo wallet to share with their contacts.', 'The contact list (demo data) allows full CRUD actions.'],
         src: 'https://nadaveliash.github.io/vitcoin-vue/#/',
-        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1715799737/vitcoin_hj9lxk.mp4',
+        video: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725219518/pc-crypto_fjqxz9.mp4',
+        mobileVideo: 'https://res.cloudinary.com/dnvbfkgsb/video/upload/v1725257970/mobile-crypto_js8dty.mp4',
         repo: 'https://github.com/NadavEliash/vitcoin-vue'
     },
 ]
@@ -86,7 +99,7 @@ export default function Projects() {
             setIsScroll(true)
             setIsReplacing(true)
 
-            ev.deltaY < 0
+            ev.deltaY > 0
                 ? setCurrentView(currentView + 1 >= projects.length ? 0 : currentView + 1)
                 : setCurrentView(currentView - 1 < 0 ? projects.length - 1 : currentView - 1)
 
--- app/components/code-project.tsx
+++ app/components/code-project.tsx
@@ -22,26 +22,28 @@ export default function CodeProject({ project }: CodeProjectProps) {
     const onLoad = () => {
         setTimeout(() => {
             setIsLoading(false)
-        }, 1000);
+        }, 600);
     }
 
 
     return (
-        <div className={`grid grid-cols-8 transition-all duration-1000 md:pt-32 h-full ${fredoka.className}`}>
-            <div className="col-span-8 md:col-span-3 lg:col-span-2 flex flex-col gap-2 md:justify-center">
+        <div className={`grid grid-cols-10 transition-all duration-1000 md:pt-28 h-full max-h-[100svh] overflow-hidden ${fredoka.className}`}>
+            <div className="col-span-10 md:col-start-2 md:col-end-5 flex flex-col gap-2 md:justify-start">
                 <video src={project.video} className="md:hidden" autoPlay loop onClick={() => router.push(project.src)}>
                 </video>
-                <div className="absolute top-[35svh] sm:top-[45svh] md:static">
+                <div className="absolute top-[38svh] md:h-[560px] sm:top-[45svh] md:static md:mt-28 flex flex-col justify-between">
                     <h1 className={`text-[2.5rem] text-center  ${fredoka.className}`}>{project.title}</h1>
                     {project.subTitle && <h2 className="mb-1 text-center">{project.subTitle}</h2>}
-                    {project.description.map((line: string, idx: number) => <h2 key={idx} className="text-sm sm:text-base xl:text-lg mx-6">{line}</h2>)}
+                    <div className="">
+                        {project.description.map((line: string, idx: number) => <h2 key={idx} className="text-sm sm:text-base xl:text-lg mx-2 text-justify">{line}</h2>)}
+                    </div>
 
                     <div id="buttons" className="flex md:flex-col gap-4 mt-6 mx-6 md:mx-10">
-                        <div id="toggle-display" className="hidden group lg:flex relative w-36 h-12 bg-white/20 rounded-full items-center justify-between p-3 cursor-pointer ml-6" onClick={() => setWideScreen(!wideScreen)}>
+                        {project.params !== 'animate' && <div id="toggle-display" className="hidden group lg:flex relative w-36 h-12 bg-white/20 rounded-full items-center justify-between p-3 cursor-pointer ml-6" onClick={() => { if (project.params === 'animate') { return } else { setWideScreen(!wideScreen) } }}>
                             <Tv2 className="w-6 h-6 group-hover:w-8 transition-all duration-300" />
                             <Smartphone className="w-6 h-6 group-hover:w-8 transition-all duration-300" />
                             <div className={`absolute ${wideScreen ? 'left-[4px]' : 'left-[100px] group-hover:left-[92px]'} transition-all duration-200 w-10 group-hover:w-12 h-10 rounded-full bg-white/20`}></div>
-                        </div>
+                        </div>}
                         <Link href={project.src} target="_blank"
                             className="group bg-white/20 rounded-full p-3 w-24 hover:w-40 transition-all duration-300 relative hover:bg-white/30 overflow-hidden md:ml-24">
                             <h1 className="">Go <span className="absolute left-0 text-transparent group-hover:left-9 group-hover:text-inherit transition-all duration-400 text-nowrap">to the App</span>
@@ -55,14 +57,17 @@ export default function CodeProject({ project }: CodeProjectProps) {
                     </div>
                 </div>
             </div>
-            <div className={`hidden md:block md:col-span-5 lg:col-span-6 justify-self-center relative opacity-90 shadow-xl shadow-white/30 scale-y-[0.7] scale-x-[0.8] transition-all duration-700
-                                    ${wideScreen ? 'w-[380px] rounded-[3rem] lg:w-full lg:max-w-5xl lg:rounded-2xl rotate-3 -skew-x-3' : 'w-[380px] rounded-[3rem] -rotate-12 -ml-40 skew-y-1'} 
-                                    ${project.title === 'Finerr' ? 'h-[700px]' : ''}`}>
-                <div className={`absolute top-3 left-3 bg-black/70 w-full h-full -z-10 ${wideScreen ? 'rounded-[3rem] lg:rounded-2xl' : 'rounded-[3rem]'}`}></div>
+            <div className={`hidden md:block md:col-span-5 lg:col-span-6 justify-self-center relative opacity-90  scale-y-[0.7] scale-x-[0.8] transition-all duration-500
+                                    ${wideScreen
+                    ? 'w-[380px] rounded-[3rem] lg:w-full lg:max-w-5xl lg:rounded-2xl rotate-3 -skew-x-3'
+                    : 'w-[380px] rounded-[3rem] -rotate-12 -ml-40 skew-y-1'}`}>
+                {/* <div className={`absolute top-3 left-3 bg-black/70 w-full h-full -z-10 ${wideScreen ? 'rounded-[3rem] lg:rounded-2xl' : 'rounded-[3rem]'}`}></div> */}
                 {isLoading && <div className={`absolute w-full h-full bg-black border-[20px] border-black ${wideScreen ? 'rounded-[3rem] lg:rounded-2xl' : 'rounded-[3rem]'} flex items-center justify-center overflow-hidden`}>
                     <Loader />
                 </div>}
-                <iframe src={project.src} onLoad={onLoad} className={`w-full h-full border-[20px] border-black bg-white ${wideScreen ? 'rounded-[3rem] lg:rounded-2xl' : 'rounded-[3rem]'}`}></iframe>
+                <video src={wideScreen ? project.video : project.mobileVideo} onCanPlay={onLoad} autoPlay loop className={`hidden lg:block border-[20px] border-slate-800  ${wideScreen ? 'rounded-[3rem] lg:rounded-2xl' : 'rounded-[3rem]'} shadow-[5px_5px_rgba(0,0,0,0.4),_10px_10px_rgba(0,0,0,0.3),_15px_15px_rgba(0,0,0,0.2),_20px_20px_rgba(0,0,0,0.1),_25px_25px_rgba(0,0,0,0.1)]`}></video>
+                <video src={project.mobileVideo} onCanPlay={onLoad} autoPlay loop className={`lg:hidden border-[20px] border-slate-800 h-full rounded-[3rem]`}></video>
+                {/* <iframe src={project.src} onLoad={onLoad} className={`w-full h-full border-[20px] border-black bg-white ${wideScreen ? 'rounded-[3rem] lg:rounded-2xl' : 'rounded-[3rem]'}`}></iframe> */}
             </div>
 
         </div>
